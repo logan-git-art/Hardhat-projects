43e6e7cec5446b473c74bf63bcdb1b18fd24f027
Chetan Bisht <84459081+logan-git-art@users.noreply.github.com>
2024-01-09 02:43:51 +0530

    Update and rename block-number.js to tasks/block-number.js

--- tasks/block-number.js
+++ tasks/block-number.js
@@ -1,8 +1,7 @@
 const { task } = require("hardhat/config")
 
 task("block-number", "Prints the current block number").setAction(
-  // const blockTask = async function() => {}
-  // async function blockTask() {}
+ 
   async (taskArgs, hre) => {
     const blockNumber = await hre.ethers.provider.getBlockNumber()
     console.log(`Current block number: ${blockNumber}`)
